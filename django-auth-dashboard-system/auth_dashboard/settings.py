ea1697153dd2fb27f1cc76aa2ee6888ac44812c2
MuhammadMoueen <s23bdocs1m01131@iub.edu.pk>
2026-02-18 23:14:51 +0500

    Restructure auth dashboard: move templates and static into users app folder

--- django-auth-dashboard-system/auth_dashboard/settings.py
+++ django-auth-dashboard-system/auth_dashboard/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'auth_dashboard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
