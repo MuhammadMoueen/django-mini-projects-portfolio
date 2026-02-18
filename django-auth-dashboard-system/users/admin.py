80beca9d44e2c14ca515176e03cec312f7dfcc49
MuhammadMoueen <s23bdocs1m01131@iub.edu.pk>
2026-02-18 23:35:36 +0500

    Customize Django admin panel branding

--- django-auth-dashboard-system/users/admin.py
+++ django-auth-dashboard-system/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+admin.site.site_header = "Auth Dashboard Admin"
+admin.site.site_title = "Auth Dashboard"
+admin.site.index_title = "Welcome to Auth Dashboard Admin Panel"
