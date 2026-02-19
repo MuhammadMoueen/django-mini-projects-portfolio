91e1be31332ef97cf82198d1f4b49562454ce0c5
MuhammadMoueen <s23bdocs1m01131@iub.edu.pk>
2026-02-19 19:33:52 +0500

    Add fieldsets to admin for better organization

--- django-user-profile-manager/users/admin.py
+++ django-user-profile-manager/users/admin.py
@@ -7,3 +7,17 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_filter = ['created_at', 'updated_at']
     search_fields = ['user__username', 'full_name', 'phone', 'location']
     readonly_fields = ['created_at', 'updated_at']
+    fieldsets = (
+        ('User Information', {
+            'fields': ('user', 'full_name', 'profile_picture')
+        }),
+        ('Contact Details', {
+            'fields': ('phone', 'location')
+        }),
+        ('Bio', {
+            'fields': ('bio',)
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at')
+        }),
+    )
