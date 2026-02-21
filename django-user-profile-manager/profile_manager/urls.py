259d427abfc4a42086cc5476f5ada884df199596
MuhammadMoueen <s23bdocs1m01131@iub.edu.pk>
2026-02-21 23:50:58 +0500

    Remove admin URL path from main urls.py

--- django-user-profile-manager/profile_manager/urls.py
+++ django-user-profile-manager/profile_manager/urls.py
@@ -1,11 +1,9 @@
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', lambda request: redirect('dashboard'), name='home'),
     path('', include('users.urls')),
 ]
