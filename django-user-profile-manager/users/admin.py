bfa2acf5a910fa729d84e80f75dd11ec78bd6b5f
MuhammadMoueen <s23bdocs1m01131@iub.edu.pk>
2026-02-23 00:10:02 +0500

    feat(admin): Register models in Django admin interface
    - Added proper admin registration for all models
    - Registered: UserProfile, Education, Skill, Experience, Project
    - Replaced placeholder comment with functional admin setup
    - Enables admin management of user data when needed

--- django-user-profile-manager/users/admin.py
+++ django-user-profile-manager/users/admin.py
@@ -1,1 +1,8 @@
-# Admin interface is not used in this project
+from django.contrib import admin
+from .models import UserProfile, Education, Skill, Experience, Project
+
+admin.site.register(UserProfile)
+admin.site.register(Education)
+admin.site.register(Skill)
+admin.site.register(Experience)
+admin.site.register(Project)
