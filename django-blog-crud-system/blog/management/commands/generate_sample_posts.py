f85d88750ab02c8933170e0409bff65a063360cb
MuhammadMoueen <s23bdocs1m01131@iub.edu.pk>
2026-02-24 00:24:30 +0500

    Organize imports in generate_sample_posts management command

--- django-blog-crud-system/blog/management/commands/generate_sample_posts.py
+++ django-blog-crud-system/blog/management/commands/generate_sample_posts.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from blog.models import Post
 import random
+
+from blog.models import Post
+
 
 class Command(BaseCommand):
     help = 'Generate sample blog posts for testing'
