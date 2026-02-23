a46f93c795ad766bc19079c936be6007a71608fc
MuhammadMoueen <s23bdocs1m01131@iub.edu.pk>
2026-02-24 00:26:46 +0500

    Add consistent blank lines in urls.py for better code structure

--- django-blog-crud-system/blog_system/urls.py
+++ django-blog-crud-system/blog_system/urls.py
@@ -3,14 +3,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 admin.site.site_header = "BlogSys Administration"
 admin.site.site_title = "BlogSys Admin Portal"
 admin.site.index_title = "Welcome to BlogSys Admin"
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
