801d5eab0cff37dc2ea7a673a1ddcfc30c3ba113
MuhammadMoueen <s23bdocs1m01131@iub.edu.pk>
2026-02-23 22:21:05 +0500

    Configure media files URL patterns

--- django-blog-crud-system/blog_system/urls.py
+++ django-blog-crud-system/blog_system/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_header = "BlogSys Administration"
 admin.site.site_title = "BlogSys Admin Portal"
@@ -9,3 +11,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
