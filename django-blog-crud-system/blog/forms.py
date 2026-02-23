0e4d3d94d712ce4e06b3188d5e4d31e126d2551a
MuhammadMoueen <s23bdocs1m01131@iub.edu.pk>
2026-02-23 23:34:53 +0500

    Add profile management forms (UserUpdate, ProfileUpdate, PasswordChange)

--- django-blog-crud-system/blog/forms.py
+++ django-blog-crud-system/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from .models import Post
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from .models import Post, UserProfile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(
@@ -21,6 +21,82 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    """Form for updating user information"""
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your email'
+        })
+    )
+    first_name = forms.CharField(
+        required=False,
+        max_length=30,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your first name'
+        })
+    )
+    last_name = forms.CharField(
+        required=False,
+        max_length=30,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your last name'
+        })
+    )
+    
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    """Form for updating user profile"""
+    bio = forms.CharField(
+        required=False,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'rows': 4,
+            'placeholder': 'Tell us about yourself...'
+        })
+    )
+    profile_pic = forms.ImageField(
+        required=False,
+        widget=forms.FileInput(attrs={
+            'class': 'form-control',
+            'accept': 'image/*'
+        })
+    )
+    
+    class Meta:
+        model = UserProfile
+        fields = ['bio', 'profile_pic']
+
+class CustomPasswordChangeForm(PasswordChangeForm):
+    """Custom password change form with styled widgets"""
+    old_password = forms.CharField(
+        label='Current Password',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter current password'
+        })
+    )
+    new_password1 = forms.CharField(
+        label='New Password',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter new password'
+        })
+    )
+    new_password2 = forms.CharField(
+        label='Confirm New Password',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Confirm new password'
+        })
+    )
 
 class PostForm(forms.ModelForm):
     class Meta:
