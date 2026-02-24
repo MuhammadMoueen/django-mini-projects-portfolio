9ce59d4144805987de4f4e8e13058cc07c424e6a
MuhammadMoueen <s23bdocs1m01131@iub.edu.pk>
2026-02-24 22:33:01 +0500

    Fix CV model save method to prevent AttributeError
    - Use explicit super(CV, self).save() instead of super().save()
    - Exclude current instance when deactivating other CVs
    - Fixes 'super object has no attribute dicts' error in admin

--- myportfolio/main/models.py
+++ myportfolio/main/models.py
@@ -25,8 +25,9 @@ class CV(models.Model):
     
     def save(self, *args, **kwargs):
         if self.is_active:
-            CV.objects.filter(is_active=True).update(is_active=False)
-        super().save(*args, **kwargs)
+            # Deactivate all other CVs before saving this one as active
+            CV.objects.filter(is_active=True).exclude(pk=self.pk).update(is_active=False)
+        super(CV, self).save(*args, **kwargs)
 
 class Project(models.Model):
     CATEGORY_CHOICES = [
